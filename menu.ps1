7bd056872a368929ec73b3fed8991e9805f7a776
Supperelias2 <36726086+Supperelias2@users.noreply.github.com>
2024-08-15 01:51:04 +0200

    added automatic elevation in the start of the script

--- menu.ps1
+++ menu.ps1
@@ -3,6 +3,18 @@ $hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
 $backupPath = "C:\Temp\hostsbackup\"
 $url = "https://a.dove.isdumb.one/list.txt"
 
+function Check-AdministratorRights {
+    # Controleer of het script als administrator wordt uitgevoerd
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        # Het script wordt niet als administrator uitgevoerd, dus start het opnieuw met verhoogde rechten
+        Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+
+        # Stop het huidige script
+        Exit
+    }
+}
+
+
 function Show-Menu {22
     Clear-Host
     Write-Host "Select an option:"
@@ -105,5 +117,7 @@ function Exit-Script {
     Exit
 }
 
+# start van het script
+Check-AdministratorRights
 # Start het menu
 Show-Menu
