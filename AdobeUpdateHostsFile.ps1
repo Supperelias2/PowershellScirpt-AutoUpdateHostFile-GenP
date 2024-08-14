64ebad8266af1c3160155771596baa88e4cffbb3
Wonder Slons <eliasverstappen@gmail.com>
2024-08-14 16:14:19 +0200

    Added backup method7

--- AdobeUpdateHostsFile.ps1
+++ AdobeUpdateHostsFile.ps1
@@ -1,6 +1,12 @@
+# Define variables for easy configuration
+$hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
+$backupPath = "C:\Temp2\hosts.bak"
+$url = "https://a.dove.isdumb.one/list.txt"
+
+
 function Test-HostsFileWritable {
     param (
-        [string]$hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
+        [string]$hostsFilePath = $hostsPath
     )
 
     try {
@@ -12,14 +18,38 @@ function Test-HostsFileWritable {
     }
 }
 
+function Backup-HostsFile {
+    param (
+        [string]$hostsPath = $hostsPath,
+        [string]$backupPath = $backupPath
+    )
+
+    try {
+        # Ensure the backup directory exists
+        $backupDir = [System.IO.Path]::GetDirectoryName($backupPath)
+        if (-not (Test-Path -Path $backupDir)) {
+            New-Item -Path $backupDir -ItemType Directory -Force | Out-Null
+        }
+
+        Copy-Item -Path $hostsPath -Destination $backupPath -Force
+        Write-Output "Backup of the hosts file created at $backupPath."
+    } catch {
+        Write-Error "Failed to create a backup of the hosts file: $_"
+    }
+}
+
 function Update-HostsFile {
     param (
-        [string]$url = "https://a.dove.isdumb.one/list.txt",
-        [string]$hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
+        [string]$url = $url,
+        [string]$hostsPath = $hostsPath,
+        [string]$backupPath = $backupPath
     )
 
     # Check if the hosts file is writable
     if (Test-HostsFileWritable -hostsFilePath $hostsPath) {
+        # Create a backup before updating
+        Backup-HostsFile -hostsPath $hostsPath -backupPath $backupPath
+
         try {
             $content = Invoke-WebRequest -Uri $url -UseBasicParsing
             if ($content.StatusCode -eq 200) {
