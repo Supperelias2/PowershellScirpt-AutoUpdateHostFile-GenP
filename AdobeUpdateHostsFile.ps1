dd638294d5241b73e9cada72fd816a66fed40229
Wonder Slons <eliasverstappen@gmail.com>
2024-08-14 16:04:12 +0200

    added automatic check if the hosts file is writable

--- AdobeUpdateHostsFile.ps1
+++ AdobeUpdateHostsFile.ps1
@@ -1,17 +1,41 @@
-# Define the URL and the path to the hosts file
-$url = "https://a.dove.isdumb.one/list.txt"
-$hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
+function Test-HostsFileWritable {
+    param (
+        [string]$hostsFilePath = "$env:SystemRoot\System32\drivers\etc\hosts"
+    )
 
-# Download the contents from the URL
-try {
-    $content = Invoke-WebRequest -Uri $url -UseBasicParsing
-    if ($content.StatusCode -eq 200) {
-        # Overwrite the hosts file with the downloaded content
-        $content.Content | Out-File -FilePath $hostsPath -Encoding ASCII
-        Write-Output "The hosts file has been successfully overwritten."
-    } else {
-        Write-Error "Failed to download content. Status Code: $($content.StatusCode)"
+    try {
+        $fileStream = [System.IO.File]::Open($hostsFilePath, 'Open', 'ReadWrite', 'None')
+        $fileStream.Close()
+        return $true
+    } catch {
+        return $false
     }
-} catch {
-    Write-Error "An error occurred: $_"
 }
+
+function Update-HostsFile {
+    param (
+        [string]$url = "https://a.dove.isdumb.one/list.txt",
+        [string]$hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
+    )
+
+    # Check if the hosts file is writable
+    if (Test-HostsFileWritable -hostsFilePath $hostsPath) {
+        try {
+            $content = Invoke-WebRequest -Uri $url -UseBasicParsing
+            if ($content.StatusCode -eq 200) {
+                # Overwrite the hosts file with the downloaded content
+                $content.Content | Out-File -FilePath $hostsPath -Encoding ASCII
+                Write-Output "The hosts file has been successfully overwritten."
+            } else {
+                Write-Error "Failed to download content. Status Code: $($content.StatusCode)"
+            }
+        } catch {
+            Write-Error "An error occurred: $_"
+        }
+    } else {
+        Write-Error "The hosts file is not writable by the current user."
+    }
+}
+
+# Execute the function to update the hosts file
+Update-HostsFile
