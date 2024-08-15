a4a52f79997f98cc75bb1e9fb98d9c0556b5cfd4
Supperelias2 <36726086+Supperelias2@users.noreply.github.com>
2024-08-15 13:41:20 +0200

    logupdate

--- GenP-UpdateHostsFile.ps1
+++ GenP-UpdateHostsFile.ps1
@@ -1,14 +1,48 @@
 # Define variables for easy configuration
 $hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
 $scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
 $backupPath = "$scriptpath\HostfileBackup\"
 $logpath = "$scriptpath\LOGS\"
 $url = "https://a.dove.isdumb.one/list.txt"
 
+function LogMessage {
+    param (
+        [string]$message
+    )
+     # Ensure that the LOGS directory exists
+     if (-not (Test-Path -Path $logpath)) {
+        try {
+            New-Item -Path $logpath -ItemType Directory -Force
+        } catch {
+            Write-Error "Failed to create LOGS directory: $_"
+            return
+        }
+    }
+
+    # Set the full path to the log file
+    $logfile = Join-Path -Path $logpath -ChildPath "log.txt"
+
+    # Get the current date and time
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    # Construct the log entry
+    $logEntry = "$timestamp - $message"
+
+    # Append the log entry to the log file with error handling
+    try {
+        Add-Content -Path $logfile -Value $logEntry
+    } catch {
+        Write-Error "Failed to write to log file: $_"
+    }
+    
+}
 function Test-AdministratorRight {
     # check if the script is run as admin
+    LogMessage "Checking if you are admin"
     if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
         # the script is not run as admin
+        LogMessage "Script not running as asdmin - Restarting Script as Admin"
         Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 
         # Stop the current script
@@ -16,8 +50,10 @@ function Test-AdministratorRight {
     }
 }
 
-function Show-Menu {22
+function Show-Menu {
     Clear-Host
+    LogMessage "Displaying menu options to the user."
+
     Write-Host "Select an option:"
     Write-host ""
     Write-Host "1. Check host file write status"
@@ -28,12 +64,27 @@ function Show-Menu {22
     Write-Host ""
     $choice = Read-Host "Enter a menu option (0-3)"
 
+    LogMessage "User selected menu option: $choice"
+
     switch ($choice) {
-        1 { Test-HostFileWriteStatus }
-        2 { Backup-CurrentHostFile }
-        3 { Update-HostFile }
-        0 { Exit-Script }
+        1 { 
+            LogMessage "User selected to check host file write status."
+            Test-HostFileWriteStatus 
+        }
+        2 { 
+            LogMessage "User selected to backup the current host file."
+            Backup-CurrentHostFile 
+        }
+        3 { 
+            LogMessage "User selected to update and overwrite the host file with new entries."
+            Update-HostFile 
+        }
+        0 { 
+            LogMessage "User selected to exit the script."
+            Exit-Script 
+        }
         Default { 
+            LogMessage "User selected an invalid option: $choice"
             Clear-host
             Write-Host "Invalid option. Please try again." -ForegroundColor Red
             Pause
@@ -42,24 +93,36 @@ function Show-Menu {22
     }
 }
 
+
 function Test-HostFileWriteStatus {
-    clear-host
+    Clear-Host
+    LogMessage "Starting to check host file write status."
+
     Write-Host "Checking host file write status..."
     $hostFilePath = "C:\Windows\System32\drivers\etc\hosts"
+
     if (Test-Path -Path $hostFilePath -PathType Leaf) {
+        LogMessage "Host file found at path: $hostFilePath"
         $fileInfo = Get-Item $hostFilePath
+
         if ($fileInfo.IsReadOnly) {
+            LogMessage "Host file is read-only. User needs to change permissions to write to it."
             Write-Host "The host file is read-only. You need to change permissions to write to it." -ForegroundColor Red
         } else {
+            LogMessage "Host file is writable."
             Write-Host "The host file is writable." -ForegroundColor Green
         }
     } else {
+        LogMessage "Host file not found at path: $hostFilePath"
         Write-Host "Host file not found." -ForegroundColor Red
     }
+
     Write-Host ""
+    LogMessage "Completed checking host file write status."
     Pause
     Show-Menu
 }
+
 
 function Backup-CurrentHostFile {
     Clear-host
